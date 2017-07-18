e6ba8d2be12884089edf65846a9f25af7bd9c37d
Lý Đông Cảnh <keshiki.ldc@gmail.com>
2017-07-18 10:23:46 +0700

    Update ISerializeHelpers.cs
    Change DeleteSaveFile return type.

--- SerializerHelpers/SerializerHelpers/ISerializeHelpers.cs
+++ SerializerHelpers/SerializerHelpers/ISerializeHelpers.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 using System.Runtime.Serialization;
 
-namespace SerializerHelpers
+namespace LINQPractice.Serializer
 {
-    public interface ISerializeHelpers
+    public interface ISerializer
     {
         bool TrySave<T>(string dataPath, string saveName, T param) where T : class;
         bool TryLoad<T>(string dataPath, string saveName, out T param) where T : class;
-        bool DeleteSaveFile(string dataPath, string saveName);
+        void DeleteSaveFile(string dataPath, string saveName);
     }
 }
