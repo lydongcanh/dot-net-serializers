fa225d29c9ae42733492f79198bc033c75aa1ebe
Lý Đông Cảnh <keshiki.ldc@gmail.com>
2017-11-03 23:34:31 +0700

    Remove unnecessary generic requirement.

--- SerializerHelpers/SerializerHelpers/ISerializeHelpers.cs
+++ SerializerHelpers/SerializerHelpers/ISerializeHelpers.cs
@@ -5,8 +5,8 @@ namespace SerializerHelpers
 {
     public interface ISerializer
     {
-        bool TrySave<T>(string dataPath, string saveName, T param) where T : class;
-        bool TryLoad<T>(string dataPath, string saveName, out T param) where T : class;
+        bool TrySave<T>(string dataPath, string saveName, T param);
+        bool TryLoad<T>(string dataPath, string saveName, out T param);
         void DeleteSaveFile(string dataPath, string saveName);
     }
 }
